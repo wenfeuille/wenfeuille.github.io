a140e81b6d0a392126846f5753752b0b6e953688
wenfeuille <wenfeiwei@gmail.com>
2017-06-26 10:10:56 -0500

    Update trial.js

--- trial.js
+++ trial.js
@@ -1,5 +1,5 @@
-$(document).ready(function(){
-    $('div').click(function(){
-        $('div').effect('bounce', {times:3}, 500);
-        });
+$(document).ready(function() {
+    $('#notready').click(function(){
+         $('#notready').fadeOut(1000);
     });
+});
