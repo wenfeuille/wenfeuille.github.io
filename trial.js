7e05ef425ceeb801cc4bcf8052b79f2a2fffb8f4
wenfeuille <wenfeiwei@gmail.com>
2017-06-26 23:59:08 -0500

    Update trial.js

--- trial.js
+++ trial.js
@@ -1,8 +1,18 @@
 $(document).ready(function(){
   var $jqName = $('.name');
   var $jqValue = $('.jqValue');
+  var stupids = ['michael', 'david', 'huy'];
+
   
   $('div').click(function(){
-        $jqValue.html('stupid' + ' ' + $jqName.val());
-  });
-});
+    for(var i = 0; i < stupids.length; i++){
+      if(stupids[i] === $jqName.val()){
+        $jqValue.html('stupid' + ' ' + stupids[i]);
+        i = stupids.length;
+      }
+      else{
+        $jqValue.html($jqName.val());
+      }
+      
+  
+    }
