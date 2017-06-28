4d8edfca194f68f33c81a6976c704a73dc5cb823
wenfeuille <wenfeiwei@gmail.com>
2017-06-27 23:09:14 -0500

    Update trial.js

--- trial.js
+++ trial.js
@@ -1,16 +1,20 @@
 $(document).ready(function(){
-  var $jqName = $('.name');
+  
   var $jqValue = $('.jqValue');
   var stupids = ['michael', 'david', 'huy', 'george'];
   var list = [];
   
   $('div').click(function(){
-
+  
+  var $jqName = $('#name option:selected').text();
+  
   for (var i = 0; i < stupids.length; i++){
-    if(stupids[i] !== $jqName.val()){
+    if(stupids[i] !== $jqName){
       list.push(stupids[i] + ' ');
     }
   }
+  
+  console.log(list);
   
   for (var i = 0; i < list.length; i++){
     $('.jqValue').append('<div>' + list[i] + '</div>');
