9ef029ed889f06ffea05670a1b867bdcd798dcac
wenfeuille <wenfeiwei@gmail.com>
2018-08-01 01:07:10 -0500

    Update trial.js

--- trial.js
+++ trial.js
@@ -8,13 +8,26 @@ $(document).ready(function(){
   
   var $jqName = $('#name option:selected').text();
   
-  $jqValue = $jqName
+  for (var i = 0; i < stupids.length; i++){
+    if(stupids[i] !== $jqName){
+      list.push(stupids[i] + ' ');
+    }
+  }
+  
+  for (i = 0; i < list.length; i++){
+    $('.jqValue').append('<div>' + list[i] + '</div>');
+  }
   
   });
   
   var form = "<form>Enter your name: <select><option>michael</option><option>david</option><option>huy</option><option>george</option></select></form>";
   var numForms = 1;
   
+  $('#addName').click(function(){
+    $('#winnie').append("<div></div>" + form); 
+    numForms++;
+    console.log(numForms);
+  });
   
 
 });
