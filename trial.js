91f32ec81c9a29673574e62d12af79191374b453
wenfeuille <wenfeiwei@gmail.com>
2018-08-09 17:22:19 -0400

    Update trial.js

--- trial.js
+++ trial.js
@@ -3,31 +3,54 @@ $(document).ready(function(){
   var $jqValue = $('.jqValue');
   var stupids = ['michael', 'david', 'huy', 'george'];
   var list = [];
+  var choice;
+  var numForms = 0;
+  var $addChoice;
+  var formNumber = 0;
+  var formID = ['choice1', 'choice2', 'choice3', 'choice4', 'choice5'];
   
-  $('#submit').click(function(){
+    var form1 = "<form id = 'choice1'>Enter your name: <select><option>klaus</option><option>david</option><option>huy</option><option>george</option></select></form>";
+    var form2 = "<form id = 'choice2'>Enter your name: <select><option>klaus</option><option>david</option><option>huy</option><option>george</option></select></form>";
+    var form3 = "<form id = 'choice3'>Enter your name: <select><option>klaus</option><option>david</option><option>huy</option><option>george</option></select></form>";
+    var form4 = "<form id = 'choice4'>Enter your name: <select><option>klaus</option><option>david</option><option>huy</option><option>george</option></select></form>";
+    var form5 = "<form id = 'choice5'>Enter your name: <select><option>klaus</option><option>david</option><option>huy</option><option>george</option></select></form>";
   
-  var $jqName = $('#name option:selected').text();
-  
-  for (var i = 0; i < stupids.length; i++){
-    if(stupids[i] !== $jqName){
-      list.push(stupids[i] + ' ');
-    }
-  }
-  
-  for (i = 0; i < list.length; i++){
-    $('.jqValue').append('<div>' + list[i] + '</div>');
-  }
-  
-  });
-  
-  var form = "<form>Enter your name: <select><option>michael</option><option>david</option><option>huy</option><option>george</option></select></form>";
-  var numForms = 1;
-  
+ var forms = [form1, form2, form3, form4, form5];
+
+      
   $('#addName').click(function(){
-    $('#winnie').append("<div></div>" + form); 
+      
+      
+    $jqValue.append(forms[formNumber]);
     numForms++;
-    console.log(numForms);
+    var element = document.getElementById("choice");
+    formNumber++;
+      
   });
+  
+  $('#add').click(function(){
+      var $form1 = $('#choice1 option:selected');
+      var $form2 = $('#choice2 option:selected');
+      var $form3 = $('#choice3 option:selected');
+      var $form4 = $('#choice4 option:selected');
+      var $form5 = $('#choice5 option:selected');
+      
+      var array = [$form1, $form2, $form3, $form4, $form5];
+      
+      for (i = 0; i < numForms; i++){
+          
+          var currentChoice = array[i].text();
+          list.push(currentChoice);
+          
+      }
+      
+      $('.test').append(list);
+  });
+  
+
+
+ 
+  
   
 
 });
